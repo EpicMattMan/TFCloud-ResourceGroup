0de5c31d6f2a485d0b940d8e36377e77576c1af2
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-04 10:35:36 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  version "<=2.0.0"
+  
+  subscription_id = var.subscriptionID
+  client_id = var.clientID
+  client_secret = var.clientSecret
+  tenant_id = var.tenantID
+  
+  features{}
+  
+}
+
+resource "azurerm_resource_group" = "tfcloud-rg" {
+  name =var.RGName
+  location = var.location
+}
