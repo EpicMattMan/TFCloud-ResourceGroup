efaa9a9d0632af2ad14a8fbfb16917abc6ebcb4f
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-07 14:27:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   
   subscription_id = var.subscription_id
   client_id = var.client_id
-  client_secret = var.client_secret
+  client_secret = var.clientSecret
   tenant_id = var.tenant_id
   
   features{}
